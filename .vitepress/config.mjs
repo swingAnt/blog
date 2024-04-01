aea84b477d0e4200f27c37b951fa8e22aabb2cde
qinyujie <943868899@qq.com>
2024-04-01 21:11:08 +0800

    init

---
+++ .vitepress/config.mjs
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "李君凯的技术博客",
+  description: "A VitePress Site",
+  themeConfig: {
+    logo: '/profile.jpeg',
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: '关于我',
+        items: [
+          { text: '个人介绍', link: '/aboutme/0' }
+        ]
+      },
+      {
+        text: 'Vue',
+        items: [
+          { text: '快速入门', link: '/vue/0' },
+          { text: '组件', link: '/vue/1' }
+        ]
+      },
+      {
+        text: 'React',
+        items: [
+          { text: '快速入门', link: '/react/0' },
+          { text: '后管', link: '/react/1' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ index.md
@@ -0,0 +1,27 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "互联网前端技术手札"
+  text: "经验教训、读书笔记、个人知识库"
+  tagline: 技术支撑业务，业务创造价值
+  image:
+    src: /profile.jpeg
+  actions:
+    - theme: brand
+      text: 开始阅读
+      link: /markdown-examples
+    - theme: alt
+      text: 联系我
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
